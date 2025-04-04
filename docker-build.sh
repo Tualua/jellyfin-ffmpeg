f1c1faae328a13146e7b542e201112e95129f287
Dmitry Popovich <dsp@mvdnet.org>
2025-04-04 15:42:08 +0800

    WiP

--- docker-build.sh
+++ docker-build.sh
@@ -297,7 +297,9 @@ prepare_extra_amd64() {
     meson setup build --buildtype release -Denable_avx512=true
     ninja -vC build
     ninja -vC build install
-
+    echo "/usr/local/lib/x86_64-linux-gnu/libvmaf.so* usr/lib/jellyfin-ffmpeg/lib" >> ${DPKG_INSTALL_LIST}
+    echo "/usr/local/lib/x86_64-linux-gnu/libvmaf.a usr/lib/jellyfin-ffmpeg/lib" >> ${DPKG_INSTALL_LIST}
+    echo "/usr/local/bin/vmaf usr/bin" >> ${DPKG_INSTALL_LIST}
     popd
     popd
     popd
